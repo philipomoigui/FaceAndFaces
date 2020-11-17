9da843ff51cfbfb1a9d9906c346c3a76383bf997
philip omoigui <philipomoigui@gmail.com>
2020-11-17 17:10:52 +0100

    Add project files.

---
+++ Faces/FaceApiTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace FaceApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var imagePath = @"";
+
+            ImageUtility imageUtil = new ImageUtility();
+            var bytes = imageUtil.ConvertToBytes(imagePath);
+
+            List<byte[]> FaceList = null;
+
+            var byteContent = new ByteArrayContent(bytes);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            using(HttpClient httpClient = new HttpClient())
+            {
+
+            }
+        }
+    }
+}
